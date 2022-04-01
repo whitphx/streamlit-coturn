1a17bae1704c4eee72f8bef1195be72f84f8416b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-04-01 22:17:50 +0900

    Initial rough impl

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
+import logging
+import os
+
 import streamlit as st
+from streamlit_coturn.server import run_coturn
+
+logging.basicConfig(level=logging.DEBUG)
 
 st.write("Hello")
+
+heroku_app_name = os.environ.get('HEROKU_APP_NAME')
+if heroku_app_name:
+    server_name = f"https://{heroku_app_name}.herokuapp.com"
+else:
+    server_name = "localhost"
+
+run_coturn(listening_port=3478, tls_listening_port=5349, fingerprint=True, lt_cred_mech=True, server_name=server_name, realm=server_name, user="quest:password")
